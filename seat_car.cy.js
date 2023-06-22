7521b7deca2e79b42572eae21e6c6f6fb2d419c3
dwightcaro <104097782+dwightcaro@users.noreply.github.com>
2023-06-22 17:46:36 +0200

    Update seat_car.cy.js

--- seat_car.cy.js
+++ seat_car.cy.js
@@ -1,8 +1,17 @@
-describe('SEAT Car Configurator', () => {
-  const carModel = 'Mii electric';
+describe('Configurar Seat Car', () => {
+  const carModel = 'Modelo Mii electric';
   let referenceId = '';
 
   before(() => {
     cy.visit('https://www.seat.com/carworlds/mii-electric.html');
+  });
+
+  it('Configurar Mii electric con opciones por defecto', () => {
+    cy.get('button[data-test="configuratorButton"]').click();
+    cy.contains('h3', carModel).click();
+    cy.get('button[data-test="addToCartButton"]').click();
+    cy.get('.sc-ekynYh.eDxhUP > span').invoke('text').then((text) => {
+      referenceId = text.trim();
+    });
   });
 });
