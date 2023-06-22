175e0295dd258eec47f7b2ec4c29ff47661cb99f
dwightcaro <104097782+dwightcaro@users.noreply.github.com>
2023-06-22 17:42:32 +0200

    Agregando Test Aldia

---
+++ seat_car.cy.js
@@ -0,0 +1,8 @@
+describe('SEAT Car Configurator', () => {
+  const carModel = 'Mii electric';
+  let referenceId = '';
+
+  before(() => {
+    cy.visit('https://www.seat.com/carworlds/mii-electric.html');
+  });
+});
